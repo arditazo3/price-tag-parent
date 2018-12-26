d3011476e7aa31523158e189131cb8c69d739e7a
trewpog <arditazo3@gmail.com>
2018-12-26 08:43:07 +0100

    1

---
+++ price-tag-backend/src/main/java/com/tx/pt/common/domain/FormatSettings.java
@@ -0,0 +1,52 @@
+package com.tx.pt.common.domain;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+/**
+ * API model for returning format settings details.
+ *
+ * @author aazo
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class FormatSettings implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String formatPaper;
+	private Integer numberCols;
+	private Integer tipologyPrice;
+	
+	private Brand brand;
+	
+	public String getFormatPaper() {
+		return formatPaper;
+	}
+	public void setFormatPaper(String formatPaper) {
+		this.formatPaper = formatPaper;
+	}
+	public Integer getNumberCols() {
+		return numberCols;
+	}
+	public void setNumberCols(Integer numberCols) {
+		this.numberCols = numberCols;
+	}
+	public Integer getTipologyPrice() {
+		return tipologyPrice;
+	}
+	public void setTipologyPrice(Integer tipologyPrice) {
+		this.tipologyPrice = tipologyPrice;
+	}
+	public Brand getBrand() {
+		return brand;
+	}
+	public void setBrand(Brand brand) {
+		this.brand = brand;
+	}
+	
+	
+}
